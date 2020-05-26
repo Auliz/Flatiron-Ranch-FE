2b3b364b6c67c93ed5a25a1a14878923f7bfc449
aseleidy <ash.etemadi@gmail.com>
2020-05-26 14:37:08 -0400

    added functions for game and user stats

--- src/auz.js
+++ src/auz.js
@@ -1,7 +1,11 @@
 let questionsAnswered = 0
-let questionsCorrect = []
+let questionsCorrect = 0
+let questionsIncorrect = 0
+let animalsIncorrect = []
 
 const answerBtns = document.querySelector('#answer-btns')
+const animalPic = document.querySelector('#animal-pic')
+
 const optionOneBtn = document.querySelector('#option1')
 const optionTwoBtn = document.querySelector('#option2')
 const optionThreeBtn = document.querySelector('#option3')
@@ -31,30 +35,60 @@ function getRandom(arr, n) {
 }
 
 function loadAnimalPicture (randArr) {
-  
+  const randomIndex = Math.floor(Math.random() * 3)
+  const animalToRender = randArr[randomIndex]
+  animalPic.src = animalToRender.image
+  animalPic.name = animalToRender.name
+  animalPic.dataset.id = animalToRender.id
+  if (Math.random() < 0.5) {
+    animalPic.className = 'animalone'
+  } else {
+    animalPic.className = 'animaltwo'
+  }
 }
 
 function loadAnswers (randArr) {
   optionOneBtn.innerHTML = randArr[0].name
+  optionOneBtn.dataset.id = randArr[0].id
+
   optionTwoBtn.innerHTML = randArr[1].name
+  optionTwoBtn.dataset.id = randArr[1].id
+  
   optionThreeBtn.innerHTML = randArr[2].name
+  optionThreeBtn.dataset.id = randArr[2].id
 }
 
 function handleAnswerBtnClick () {
   answerBtns.addEventListener('click', event => {
-    if (event.target.id === 'option1') {
+    if (event.target.tagName === 'BUTTON' && questionsAnswered < 5) {
+      questionsAnswered++;
       checkAnswer(event);
-    } else if (event.target.id === 'option2') {
-      checkAnswer(event);
-    } else if (event.target.id === 'option3') {
-      checkAnswer(event);
+      updateGameStats();
+      updateUserStats();
+    } else if (questionsAnswered === 5) {
+      if (event.target.tagName === 'BUTTON') {
+        console.log('GAME OVER')
+        
+      }
     }
   })
 }
 
 function checkAnswer (event) {
-  console.log(getRandom(animals, 3))
-  // questionsAnswered++;
+  if (event.target.dataset.id == animalPic.dataset.id) {
+    questionsCorrect++;
+    getRandom(animals, 3);
+
+    console.log('CORRECT');
+  } else {
+    questionsIncorrect++;
+    animalsIncorrect.push({
+      userPicked: event.target.innerHTML,
+      correctAnswer: animalPic.name
+      })
+    getRandom(animals, 3);
+    console.log('WRONG')
+  } 
 }
 
 auzMain();
